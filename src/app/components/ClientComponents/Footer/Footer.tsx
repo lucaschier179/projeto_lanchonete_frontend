46f8ff2e7512c86bc1a369e08601af82cd3d7194
Lucas Marcelo Schier <ens-lucasschier@ugv.edu.br>
2024-11-10 20:59:15 -0300

    feat: :art: Criando o componente Footer

---
+++ src/app/components/ClientComponents/Footer/Footer.tsx
@@ -0,0 +1,29 @@
+import { Divider } from "@nextui-org/divider";
+
+export default function Footer() {
+  return (
+    <>
+      <main className="w-3/4 mt-10 m-auto flex flex-col gap-10">
+        <Divider />
+        <section className="flex justify-around">
+          <div className="flex flex-col gap-10">
+            <p>
+              Prato Rápido
+            </p>
+            <p>
+              Fale conosco
+            </p>
+            <p>
+              Sobre nós
+            </p>
+          </div>
+          <div>
+            <p>
+              Social
+            </p>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
