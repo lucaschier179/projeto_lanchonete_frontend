c05ac4e7b774cf807b72197df548d265f8118496
Lucas Marcelo Schier <ens-lucasschier@ugv.edu.br>
2024-11-10 20:58:57 -0300

    feat: :truck: Adicionando imagem com banner na home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
-import "../../public/images/logo_prato_rapido.png"
-import Link from "next/link";
-import Header from "./components/ClientComponents/Forms/Header/Header";
+import Footer from "./components/ClientComponents/Footer/Footer";
+import Header from "./components/ClientComponents/Header/Header";
+
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="flex flex-col items-center mt-20 mx-10">
+        <Image
+          src={"/images/banner_home.png"}
+          alt="banner home"
+          width={1316}
+          height={262}
+          className="object-cover"
+        />
+      </main>
+      <Footer />
     </>
   );
 }
