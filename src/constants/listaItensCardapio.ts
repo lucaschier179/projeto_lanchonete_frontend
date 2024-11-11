2f94a6473cd16bfd601f48e4816d76b781788423
Lucas Marcelo Schier <ens-lucasschier@ugv.edu.br>
2024-11-10 23:29:51 -0300

    feat: :art: Criando listagem estática de produtos

---
+++ src/constants/listaItensCardapio.ts
@@ -0,0 +1,60 @@
+import IListaItensCardapioData from "@/interfaces/IListaItensCardapioData";
+
+export const LISTA_ITENS_CARDAPIO: IListaItensCardapioData[] = [
+  {
+    id: 1,
+    nome: "Pastel de carne",
+    ingredientes: "massa, carne moída, cebola, salsinha, alho, ovo",
+    preco: 10.00,
+    imagem: "/images/pastel_carne.jpg"
+  },
+  {
+    id: 2,
+    nome: "Hambúrguer",
+    ingredientes: "pão, hambúrguer, queijo, alface, tomate e molho especial",
+    preco: 30.00,
+    imagem: "/images/hamburguer.webp"
+  },
+  {
+    id: 3,
+    nome: "Coxinha",
+    ingredientes: "massa, frango, catupiry",
+    preco: 10.00,
+    imagem: "/images/coxinha.webp"
+  },
+  {
+    id: 4,
+    nome: "Pizza Pepperoni",
+    ingredientes: "massa, queijo, pepperoni, ",
+    preco: 60.00,
+    imagem: "/images/pizza_pepperoni.jpg"
+  },
+  {
+    id: 5,
+    nome: "Pastel de carne",
+    ingredientes: "massa, carne moída, cebola, salsinha, alho, ovo",
+    preco: 10.00,
+    imagem: "/images/pastel_carne.jpg"
+  },
+  {
+    id: 6,
+    nome: "Hambúrguer",
+    ingredientes: "pão, hambúrguer, queijo, alface, tomate e molho especial",
+    preco: 30.00,
+    imagem: "/images/hamburguer.webp"
+  },
+  {
+    id: 7,
+    nome: "Coxinha",
+    ingredientes: "massa, frango, catupiry",
+    preco: 10.00,
+    imagem: "/images/coxinha.webp"
+  },
+  {
+    id: 8,
+    nome: "Pizza Pepperoni",
+    ingredientes: "massa, queijo, pepperoni, ",
+    preco: 60.00,
+    imagem: "/images/pizza_pepperoni.jpg"
+  },
+]
